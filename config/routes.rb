fa9ee22193ac6c42bf90e94a5b4f2294b59266e6
Isabel Chan <isabelchan1994@gmail.com>
2018-04-04 16:29:46 +0800

    rails-yelp-mvp

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :restaurants do
+    resources :reviews, only: [:new, :create]
+    # nested (for two different controller)
+  end
+
+  root to: "restaurants#index"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Creating 5 fake restaurants'
+5.times do
+  restaurant = Restaurant.new(
+    name:   Faker::Food.dish,
+    address: Faker::Address.street_address,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample,
+    phone_number: Faker::PhoneNumber.phone_number
+  )
+  restaurant.save!
+end
+puts 'Finished!'
+
